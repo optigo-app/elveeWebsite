f7e237cb746424fea2ae8136d47e66439608be09
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-05-18 10:16:29 +0530

    bug solved

--- src/ALL-Theme/SMININGROCKS/Pages/Components/home/Header/Header.js
+++ src/ALL-Theme/SMININGROCKS/Pages/Components/home/Header/Header.js
@@ -583,7 +583,7 @@ export default function Header() {
     }
 
     console.log('finalData', finalData);
-    navigation("/productpage", { state: { menuFlag: true, filtervalue: finalData } })
+    // navigation("/productpage", { state: { menuFlag: true, filtervalue: finalData } })
 
 
     if (finalData) {
@@ -603,10 +603,11 @@ export default function Header() {
           let autoCodeList = JSON.parse(localStorage.getItem("autoCodeList"))
           await getDesignPriceList(finalData, 1, {}, {}, autoCodeList).then((res) => {
             if (res) {
+              debugger
               // console.log("test",res);
               localStorage.setItem("getPriceData", JSON.stringify(res))
               // navigation(`/productpage/?${finalData?.FilterKey}=${finalData?.FilterVal}/${finalData?.FilterKey1}=${finalData?.FilterVal1}/${finalData?.FilterKey2}=${finalData?.FilterVal2}`, { state: { menuFlag: finalData?.menuname, filtervalue: finalData } })
-              navigation(`/productpage`, { state: { menuFlag: finalData?.menuname, filtervalue: finalData } })
+                navigation(`/productpage`, { state: { menuFlag: finalData?.menuname, filtervalue: finalData }})
             }
             setTimeout(() => {
               setDrawerOpen(false);
--- src/ALL-Theme/SMININGROCKS/Pages/Components/productPage/ProductList.js
+++ src/ALL-Theme/SMININGROCKS/Pages/Components/productPage/ProductList.js
@@ -2152,7 +2152,6 @@ const ProductList = () => {
   useEffect(() => {
     if ((newProData?.length != 0 || ProductApiData2?.length != 0)) {
       setIsProdLoading(true)
-      console.log('first++++');
     } else {
       if (newProData?.length == 0 || ProductApiData2?.length == 0) {
         setTimeout(() => {
@@ -2162,7 +2161,7 @@ const ProductList = () => {
         setIsProdLoading(false);
       }
     }
-  }, [newProData, ProductApiData2, priceDataApi])
+  }, [newProData, ProductApiData2])
 
   console.log('proDcount--', prodCount);
 
